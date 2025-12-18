24d1ba12db24313ad779d47b2b1e070529440a54
ggary-afk <ggary@terpmail.umd.edu>
2025-12-18 14:59:07 -0500

    I had to make some formatting changes in order to allow for more than one turn

--- main.py
+++ main.py
@@ -3,65 +3,72 @@
 #Import random module to give us our random number 
 
 import random
-
 def roll_white_dice(amount):
     rolls=[]
     for i in range(amount):
         roll = random.randint(1,12)
         rolls.append(roll)
     return rolls
-
 def roll_crow_die():
-    return random.randint(1,12) 
-
-#locking dice
-#show the user their hand
+    return random.randint(1,12)
 hand = roll_white_dice(3)
-print("Current white dice", hand)
-#make a list for locked dice
 locked_dice = []
+roll_count = 1
+print ("Roll {roll_count}")
+print("Current white dice:", hand)
+while roll_count <4:
+    print(f"\nYou are about to start roll number {roll_count + 1}")
 
-user_lock = input("Which dice would you like to lock? write like (1 3)")
-for position in user_lock.split():
-    number = int(position)
+    user_choice = input("Type in roll if you want to keep rolling or score if you want to stop:")
+    if user_choice == 'score':
+        print("chose to score")
+        break
 
-    index = number- 1
+    #locking dice
+    #show the user their hand
 
-#raise an issue if the user types in an incorrect number
-    if index < 0 or index >= len(hand):
-        print(f"warning {number} is not a position in your hand")
-        continue
-    value = hand[index]
-    locked_dice.append(value)
+    user_lock = input("Which dice would you like to lock? write like (1 3)")
+    for position in user_lock.split():
+        number = int(position)
 
-  
-#print the message
-print("you locked", locked_dice)
+        index = number- 1
 
-#calculate the size of the roll 
-dice_to_roll = len(hand) - len(locked_dice) + 2
-#roll the new dice
-hand = roll_white_dice(dice_to_roll)
-crow_die = roll_crow_die()
+    #raise an issue if the user types in an incorrect number
+        if index < 0 or index >= len(hand):
+            print(f"warning {number} is not a position in your hand")
+            continue
+        value = hand[index]
+        locked_dice.append(value)
 
-print("new white dice", hand)
-print("crow die", crow_die)
-print("locked dice", locked_dice)
+    
+    #print the message
+    print("you locked", locked_dice)
 
-#build the crow rule
+    #calculate the size of the roll 
+    dice_to_roll = len(hand) - len(locked_dice) + 2
+    #roll the new dice
+    hand = roll_white_dice(dice_to_roll)
+    crow_die = roll_crow_die()
 
-if crow_die == 12:
-    print("You rolled a 12 on the crow die, you've busted!")
+    print("new white dice", hand)
+    print("crow die", crow_die)
+    print("locked dice", locked_dice)
 
-#Account for other numbers on the crow die
-else:
-    survivors = []
-    for die in hand:
-        if die == crow_die:
-            print(f"The crow die you rolled is {crow_die} and it eliminates your white {die} ")
-        else:
-            survivors.append(die)
-    hand = survivors
-print("Your remaining white dice", hand)
+    #build the crow rule
 
+    if crow_die == 12:
+        print("You rolled a 12 on the crow die, you've busted!")
+        break
 
+    #Account for other numbers on the crow die
+    else:
+        survivors = []
+        for die in hand:
+            if die == crow_die:
+                print(f"The crow die you rolled is {crow_die} and it eliminates your white {die} ")
+            else:
+                survivors.append(die)
+        hand = survivors
+    print("Your remaining white dice", hand)
+    #update the count
+    roll_count = roll_count + 1
