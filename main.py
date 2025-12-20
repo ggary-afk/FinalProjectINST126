efed8c884fff1b4305a02659b42c30c1537e8113
ggary-afk <ggary@terpmail.umd.edu>
2025-12-19 21:34:41 -0500

    Added in the sets feature

--- main.py
+++ main.py
@@ -11,19 +11,63 @@ def roll_white_dice(amount):
     return rolls
 def roll_crow_die():
     return random.randint(1,12)
+def check_for_sets(current_dice):
+    print(f"Checking for Sets in: {current_dice}")
+
+    wild_count = 0
+    for die in current_dice:
+        if die == 12:
+            wild_count = wild_count + 1
+            
+    found_valid_set = False
+    for number_check in range(1, 12):
+        count_for_number = 0
+        for die in current_dice:
+            if die == number_check:
+                count_for_number = count_for_number + 1
+        total_count = count_for_number + wild_count
+        if total_count >= 3 and count_for_number > 0:
+            print(f"You can score a set of {number_check}s total dice: {total_count}")
+            found_valid_set = True
+
+    if found_valid_set == False:
+        print("No valid sets found")
 hand = roll_white_dice(3)
 locked_dice = []
 roll_count = 1
 print ("Roll {roll_count}")
 print("Current white dice:", hand)
+#set up check
+has_scored = False
 while roll_count <4:
     print(f"\nYou are about to start roll number {roll_count + 1}")
 
     user_choice = input("Type in roll if you want to keep rolling or score if you want to stop:")
     if user_choice == 'score':
         print("chose to score")
-        break
+        all_dice = hand + locked_dice
+        print("final dice to score:",all_dice)
+#Count the wilds
+        wild_count = 0
+        for die in all_dice:
+            if die == 12:
+                wild_count = wild_count + 1
+#check Status
+        found_valid_set = False
+#Check the range
+        for number_check in range(1,12):
+            count_for_number = 0
+            for die in all_dice:
+                if die == number_check:
+                    count_for_number = count_for_number + 1
 
+            total_count = count_for_number + wild_count
+            if total_count >= 3 and count_for_number > 0:
+                print(f"You can score a set of {number_check}s total dice: {total_count}")
+                found_valid_set = True
+        if not found_valid_set:
+            print("Sorry, you don't have a valid set")   
+        break
     #locking dice
     #show the user their hand
 
