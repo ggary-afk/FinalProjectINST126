e659fd56bd20f0e3c9da6f2f35093c5a7741473d
ggary-afk <ggary@terpmail.umd.edu>
2025-12-21 13:59:38 -0500

    Final touches and graph

--- README.md
+++ README.md
@@ -5,3 +5,25 @@
 # Game Designer: Kenna Alexander
 
 # game Website: https://coyoteandcrow.net/
+
+Features:
+1)This game has a simulated 12 sided dice roll for multiple dice.
+2)There is a locking system that allows players to build their hand with dice that they want.
+3)There is a 12 sided crow dice that will eliminate the same number dice, or will completely end your turn if it lands on 12.
+4)You will score with sets of 3 or more of the same dice or with runs of 3 or more consecutive numbers.
+5)Rolling a white 12 will act as a wild that can be used to complete runs or sets.
+6)A graph will be generated and will visualize your run or set.
+
+Requirements: 
+1)You need to have matplotlib and Python installed.
+2)launch the program by typing python main.py into your terminal.
+
+How to play:
+1:The game begins by rolling three white dice.
+2:You will get asked if you want to roll to continue receiving dice. 
+3:You can also score if you already have a run or set that you are happy with, please make sure you only type roll or score into your console.
+4:You will have to lock at least one dice from your hand each round to save a desired die for further rounds.
+5:You must type in the position of the dice that you want to lock followed by a space and another position for multiple dice like (1 3) for the first and third positioned dice.
+6:If the crow die rolls a 12 then you bust and score nothing
+7:If the crow die rolls and matches one of your dice then that white die will be removed.
+8:The game ends after 3 rolls or will end when you choose to score, it will then check your hand for runs and sets
--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 
 import random
 import time
+import matplotlib.pyplot as plt
 def roll_white_dice(amount):
     rolls=[]
     for i in range(amount):
@@ -30,9 +31,8 @@ def check_for_sets(current_dice):
         if total_count >= 3 and count_for_number > 0:
             print(f"You can score a set of {number_check}s total dice: {total_count}")
             found_valid_set = True
-
-        if found_valid_set == False:
-            print("No valid sets found")
+    if found_valid_set == False:
+        print("No valid sets found")
 def check_for_runs(current_dice):
     print(f"Checking for Runs 9 in {current_dice}")
 
@@ -47,7 +47,7 @@ def check_for_runs(current_dice):
         current_streak = 0 
         placeholder_wild = wild_count
         run_numbers = []
-        for next_num in range(start_number, 12):
+        for next_num in range(start_number, 13):
             has_number = False
             for die in current_dice:
                 if die == next_num:
@@ -66,48 +66,59 @@ def check_for_runs(current_dice):
         if current_streak >= 3:
             print(f"You have a run that starts at {start_number}: {run_numbers}")
             found_run = True
-        if found_run ==  False:
-            print("No runs found")
+    if found_run ==  False:
+        print("No runs found")
+
+def final_score_graph(final_hand):
+    print("Final scoresheet")
+    counts = [0] * 13
+    for die in final_hand:
+        counts[die] = counts[die] + 1
+    x_numbers = range(1,13)
+    y_counts = counts[1:13]
+    plt.figure(figsize=(8,5))
+    bars = plt.bar(x_numbers, y_counts, color='blue', edgecolor='black')
+    for i in range(12):
+        if y_counts[i] >= 3:
+            bars[i].set_color('green') 
+            bars[i].set_edgecolor('black')
+        elif y_counts[i] > 0:
+            bars[i].set_color('blue')
+    plt.title('Your final hand', fontsize=14)
+    plt.xlabel('Dice Number (1-12)', fontsize=12)
+    plt.ylabel('Count', fontsize=12)
+    plt.xticks(range(1, 13)) 
+    plt.yticks(range(0, 10)) 
+    plt.grid(axis='y', alpha=0.3)
+    
+    plt.savefig('final_score.png')
+
+
 hand = roll_white_dice(3)
 locked_dice = []
 roll_count = 1
-print ("Roll {roll_count}")
+print (f"Roll {roll_count}")
 print("Current white dice:", hand)
 #set up check
 has_scored = False
 while roll_count <4:
+    print(f"\n\n========================================================================")
     print(f"\nYou are about to start roll number {roll_count + 1}")
 
-    user_choice = input("Type in roll if you want to keep rolling or score if you want to stop:")
+    user_choice = input("\nType in roll if you want to keep rolling or score if you want to stop:")
     if user_choice == 'score':
         print("Chose to score")
         all_dice = hand + locked_dice
         print("Final dice to score:",all_dice)
-#Count the wilds
-        wild_count = 0
-        for die in all_dice:
-            if die == 12:
-                wild_count = wild_count + 1
-#check Status
-        found_valid_set = False
-#Check the range
-        for number_check in range(1,12):
-            count_for_number = 0
-            for die in all_dice:
-                if die == number_check:
-                    count_for_number = count_for_number + 1
+        check_for_sets(all_dice)
+        check_for_runs(all_dice)
 
-            total_count = count_for_number + wild_count
-            if total_count >= 3 and count_for_number > 0:
-                print(f"You can score a set of {number_check}s total dice: {total_count}")
-                found_valid_set = True
-        if not found_valid_set:
-            print("Sorry, you don't have a valid set")   
+        has_scored = True
         break
     #locking dice
     #show the user their hand
 
-    user_lock = input("Which dice would you like to lock? Type the position of the dice in your hand like 1 or 2:")
+    user_lock = input("You have to lock at least one die Which dice would you like to lock? Type the position of the dice in your hand like 1 or 2:")
     for position in user_lock.split():
         number = int(position)
 
@@ -125,25 +136,25 @@ while roll_count <4:
     print("You locked", locked_dice)
 
     #calculate the size of the roll 
-    dice_to_roll = len(hand) - len(locked_dice) + 2
+    dice_to_roll = len(hand) - len(user_lock.split()) + 2
     print("\nRolling dice...")
     time.sleep(3)
     #roll the new dice
     hand = roll_white_dice(dice_to_roll)
     crow_die = roll_crow_die()
 
-    print("New white dice", hand)
-    print("now here's the kicker")
-    time.sleep(4)
-    print("Crow die", crow_die)
-    print("Locked dice", locked_dice)
+    print("\nNew white dice", hand)
+    print("\nnow here's the kicker")
+    time.sleep(3)
+    print("\nCrow die", crow_die)
+    print("\nLocked dice", locked_dice)
 
     #build the crow rule
 
     if crow_die == 12:
         print("You rolled a 12 on the crow die, you've busted!")
+        has_scored = True
         break
-
     #Account for other numbers on the crow die
     else:
         survivors = []
@@ -156,3 +167,11 @@ while roll_count <4:
     print("Your remaining white dice", hand)
     #update the count
     roll_count = roll_count + 1
+
+if has_scored == False: 
+    print('End of turn, score is:')
+    all_dice = hand + locked_dice
+    check_for_sets(all_dice)
+    check_for_runs(all_dice)
+all_dice = hand + locked_dice
+final_score_graph(all_dice)
